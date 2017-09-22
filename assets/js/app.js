cdfb8b3b052e60fdecfd04f632ed26fea5e8e7e5
ksbains <k1bains@ucsd.edu>
2017-09-21 19:15:30 -0700

    still can't get firebase to work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,3 @@
-function sendChatMessage(){
-  var ref = firebase.database().ref("/chat");
-  var messageField = $('#chat-message');
-  var playerName = $("#player-name");
-  ref.push().set({
-    name: playerName.value();
-    message: messageField.value();
-  });
-
-  ref.on("child-added", function(snapshot) {
-    var message = snapshot.val();
-    addChatMessage(message.name, message.message)
-  });
-}
-
-function addChatMessage(name, message){
-  $("#chatbox").append("<p>" + name + ": " + message +" </p>");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Initialize Firebase
     var config = {
@@ -44,3 +10,118 @@ function addChatMessage(name, message){
     };
     firebase.initializeApp(config);
     var db = firebase.database();
+
+function player (name, wins, losses, choice){ 
+  this.name = name;
+  this.wins = wins;
+  this.losses = losses;
+  this.choice = choice;
+  this.fbKey = "";
+
+  this.changeName = function(name){
+    this.name = name;
+  }
+  this.win = function(){
+    this.wins = this.wins++;
+  }
+  this.lose = function(){
+    this.losses = this.losses++;
+  }
+  this.changeChoice = function(choice){
+    this.choice = choice;
+    console.log("the choice is "+choice);
+  }
+  this.getName = function(){
+    return this.name;
+  }
+}
+
+var game = {
+  player1:"",
+  player2:"",
+
+  setplayer1: function(name){
+     this.player1 = new player(name, 0, 0, ""); 
+      var ref = firebase.database().ref("/players");   
+     
+      var p1Key = ref.push({
+        name: this.player1.name,
+        wins: this.player1.wins,
+        losses:this.player1.losses,
+        choice: this.player1.choice
+      }).key;
+
+      this.player1.fbKey = p1Key;
+     return this.player1;
+  },
+  
+  setplayer2: function(name){
+     this.player2 = new player(name, 0, 0, ""); 
+     var ref = firebase.database().ref("/players");
+     
+     var p2Key = ref.push({
+        name: this.player2.name,
+        wins: this.player2.wins,
+        losses:this.player2.losses,
+        choice: this.player2.choice
+      }).key;
+
+      this.player2.fbKey = p2Key;
+     return this.player2;
+  },
+  sendChatMessage: function(player){
+    // var newRef = myDataRef.push(...);
+    // var newID = newRef.name();
+      var ref = firebase.database().ref("/chat");
+      var messageField = $('#chat-message').val().trim();
+      var playerName = player.name;
+      
+      var chatKey = ref.push({
+        name: playerName,
+        message: messageField,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+      }).key;
+      
+      //get the message just pushed to firebase and pass to addChatMessage
+      ref.on("child-added", function(snapshot) {
+        var message = snapshot.val();
+        addChatMessage(message.name, message.message)
+      });
+  }  
+}
+
+function addChatMessage(name, message){
+  $("#chatbox").append("<p>" + name + ": " + message +" </p>");
+}
+
+
+$(document).ready(function(){
+  //THE ON CLICK FUNCTIONS GO HERE 
+  $("#addPlayer").on("click", function(event){
+    event.preventDefault();
+
+    
+    var name = $("#player-name").val().trim();
+    var p1  = game.setplayer1(name);
+    //lets push this guy to the db
+    
+
+    $("#p1").empty();
+    $("#p1").prepend(p1.name);
+    //add the rest of the tuff like the rock paper scisciors. 
+    var rockDiv = $("<h2>" + "Rock" + "<h2>");
+    var paperDiv = $("<h2>" + "Paper" + "<h2>");
+    var scissorDiv = $("<h2>" + "Scissor" + "<h2>");
+    // not sure how to set the on click to not run during in line code. 
+    $("#p1").append(rockDiv).on("click", p1.changeChoice("rock"));
+    $("#p1").append(paperDiv).on("click", p1.changeChoice("paper"));
+    $("#p1").append(scissorDiv).on("click", p1.changeChoice("scissor"));
+  });
+
+  $("#addMessage").on("click", function(){
+    game.sendChatMessage(game.player1);
+  });
+});
+
+
+
